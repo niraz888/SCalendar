3437a1cce16f957651f3bd6c6098e4e527d8cde7
nir <nir.azaria@fitness360now.com>
2020-04-25 16:58:35 +0300

    install lib + first_check

--- smart_search.py
+++ smart_search.py
@@ -1,18 +1,30 @@
 from flask import Blueprint
+import requests
+from bs4 import BeautifulSoup
+
 bp = Blueprint('bp', __name__)
 
+url = 'https://www.imdb.com/search/title/?title_type=feature,tv_series'
 
-def class movie(object):
-    def __init__(self, name, genere, year):
+class movie(object):
+    def __init__(self, name, genere, year, duration):
         self.name = name
         self.genre = genere
         self.year = year
+        self.duration = duration
 
-def class IMDB_Scraper(object):
+class IMDB_Scraper(object):
     def __init__(self):
-        self.soup = Bea
+        page = requests.get(url)
+        self.soup = BeautifulSoup(page.content, 'lxml')
+
+    def check(self):
+        content = self.soup.find('h1', class_="header")
+        text = content.text
+        return text
+
 @bp.route('/bp/try')
 def first_try():
-    a = 3
-    b = 'hello'
-    return 'hello danny'
+    scraper = IMDB_Scraper()
+    text = scraper.check()
+    return text
